b9dcaf9c3a47ff2485eecc35156ca8c3625c077b
Thien Truong <88562209+ZxwinxZ@users.noreply.github.com>
2021-08-10 01:18:05 -0700

    Update Wheatstone_inductors.m

--- Mae40/Wheatstone_inductors.m
+++ Mae40/Wheatstone_inductors.m
@@ -2,6 +2,7 @@
 % Computes the currents and intermediate nodal voltages of a Wheatstone Bridge with two inductors.
 % Written by Thien Truong, modified from the code by T Bewley provided at the RR codebase.
 
+% Let x = {I0, I1, I2, I3, I4, I5, Va, Vb}
 syms L5 s V0; R1=1e3; R4=1e3; L2=1e-5; R3=1e5;
 A=[1 -1 -1 0 0 0 0 0; 0 0 1 1 0 -1 0 0; 0 1 0 -1 -1 0 0 0; 0 R1 0 0 0 0 1 0; ...
    0 0 L2*s 0 0 0 0 1; 0 0 0 0 0 L5*s 0 -1; 0 0 0 0 R4 0 -1 0; 0 0 0 R3 0 0 -1 1];
